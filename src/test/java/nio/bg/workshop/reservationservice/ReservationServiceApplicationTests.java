12ab8d1f0ce8823b2c0b03bae18085d701c2e6e1
Emanuel BG <barreragz.emanuel@gmail.com>
2020-04-23 11:11:49 -0500

    Init

---
+++ src/test/java/nio/bg/workshop/reservationservice/ReservationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package nio.bg.workshop.reservationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
